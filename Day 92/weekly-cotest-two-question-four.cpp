3c81e8400f4c067933f3e2fa4a0163c96c264927
Imran-Hossan-Rahul <imranhossanrahul.dev@gmail.com>
2026-08-28 19:09:58 +0600

    weekly contest two question no four

---
+++ Day 92/weekly-cotest-two-question-four.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    
+    
+    int t, n; cin >> t;
+    
+    
+    for(int i=0; i<t; i++){
+        
+        cin >> n;
+        
+        cout << (n * (n - 1)) / 2 << endl;
+    }
+    
+    
+    return 0;
+}
