7265fbb0d592c56835a9d26d208b9f02370e83a4
Imran-Hossan-Rahul <imranhossanrahul.dev@gmail.com>
2026-09-19 05:30:23 +0600

    Leetcode 75 sort colors (sort array with 0s, 1s and 2s) solved successfully with slightly better approach

--- Day 114/leetcode-75-sort-colors-sort-array-with-zeros-ones-and-two.cpp
+++ Day 114/leetcode-75-sort-colors-sort-array-with-zeros-ones-and-two.cpp
@@ -4,9 +4,37 @@
 using namespace std;
 
 
-
+//Sorting with brute force approach; Time complexity 0(nlogn), Space Complexity 0(1);
 void sortBruteForce(vector<int>& arr){
     sort(arr.begin(), arr.end());
+}
+
+
+//Sorting with slightly better approach;
+int sortSlighlyBetter(vector<int>& arr, int n){
+
+    int count0 = 0, count1 = 0, count2 = 0, idx = 0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] == 0){
+            count0++;
+        } else if(arr[i] == 1){
+            count1++;
+        } else{
+            count2++;
+        }
+    }
+
+    //placing the value with their count;
+    for(int i=0; i<count0; i++){
+        arr[idx++] = 0;
+    }
+    for(int i=0; i<count1; i++){
+        arr[idx++] = 1;
+    }
+    for(int i=0; i<count2; i++){
+        arr[idx++] = 2;
+    }
 }
 
 
@@ -17,7 +45,10 @@ int main(){
     int n = arr.size();
 
     //Sort with brute force
-    sortBruteForce(arr);
+    // sortBruteForce(arr);
+
+    //Sort with slightly better approach
+    sortSlighlyBetter(arr, n);
     for(int i=0; i<n; i++){
         cout << arr[i] << ", ";
     }
