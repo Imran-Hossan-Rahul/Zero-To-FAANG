4fd2d241729c4abf713f2f8401831cac94bfb296
Imran-Hossan-Rahul <imranhossanrahul.dev@gmail.com>
2026-09-19 01:27:10 +0600

    Selection Descending sorting algorithm learned successfully

--- Day 114/selection-sort.cpp
+++ Day 114/selection-sort.cpp
@@ -23,12 +23,40 @@ void selectionSortAscending(vector<int>& arr){
 }
 
 
+//Selection Descending Sorting
+void selectionSortDescending(vector<int>& arr){
+    int n = arr.size();
+
+    for(int i=0; i<n-1; i++){
+        int biggestIdx = i;
+
+        for(int j=i+1; j<n; j++){
+            if(arr[j] > arr[biggestIdx]){
+                biggestIdx = j;
+            }
+        }
+
+        if(biggestIdx != i){
+            swap(arr[biggestIdx], arr[i]);
+        }
+    }
+}
+
+
 int main(){
 
     vector<int>arr = {5, 2, 4, 2, 1, 9, 8, 10};
 
     //selection ascending sorting
     selectionSortAscending(arr);
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << ", ";
+    }
+    cout << endl;
+
+
+    //selection Descinding sorting
+    selectionSortDescending(arr);
     for(int i=0; i<arr.size(); i++){
         cout << arr[i] << ", ";
     }
